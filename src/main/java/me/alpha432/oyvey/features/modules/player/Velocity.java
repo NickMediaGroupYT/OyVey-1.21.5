66d32b1fbc59d85594c04a6537336d8588b5d36f
rtoue <rtoue@NICKPC>
2025-05-24 18:32:06 -0400

    OyVey ported to 1.21.5 working on adding modules as of right now

--- src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
@@ -1,7 +1,6 @@
 package me.alpha432.oyvey.features.modules.player;
 
-import me.alpha432.oyvey.event.EventHandler;
-import me.alpha432.oyvey.event.Listener;
+import com.google.common.eventbus.Subscribe;
 import me.alpha432.oyvey.event.impl.PacketEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
@@ -13,98 +12,98 @@ import net.minecraft.util.math.Vec3d;
 public class Velocity extends Module {
 
     public enum Mode {
-        NORMAL,
+        CANCEL,
+        REDUCE,
+        LEGIT,
         GRIM,
-        GRIM_V3,
-        WALLS,
-        NCP
+        WALLS
     }
 
-    private final Setting<Enum<?>> mode = register(new Setting<>("Mode", Mode.NORMAL));
-    private final Setting<Integer> horizontal = register(new Setting<>("Horizontal", 0, 0, 100));
-    private final Setting<Integer> vertical = register(new Setting<>("Vertical", 0, 0, 100));
-    private final Setting<Integer> ncpHorizontal = register(new Setting<>("NCP_Horizontal", 0, 0, 100));
-    private final Setting<Integer> ncpVertical = register(new Setting<>("NCP_Vertical", 0, 0, 100));
-    private final Setting<Boolean> cancelExplosions = register(new Setting<>("CancelExplosions", true));
-
-    private boolean wasHurt = false;
+    private final Setting<Enum<?>> mode = register(new Setting<>("Mode", Mode.CANCEL));
+    private final Setting<Integer> horizontal = register(new Setting<>("Horizontal%", 0, 0, 100));
+    private final Setting<Integer> vertical = register(new Setting<>("Vertical%", 0, 0, 100));
+    private final Setting<Boolean> noPush = register(new Setting<>("NoPush", true));
 
     public Velocity() {
-        super("Velocity", "Modifies player velocity packets", Category.PLAYER, true, false, false);
+        super("Velocity", "Controls knockback and push", Category.PLAYER, true, false, false);
     }
 
-    @EventHandler
-    private final Listener<PacketEvent.Receive> onPacketReceive = event -> {
-        if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket packet) {
-            if (packet.getEntityId() == mc.player.getId()) {
-                if (mode.getValue() == Mode.NORMAL) {
-                    double velX = packet.getVelocityX() / 8000.0 * (horizontal.getValue() / 100.0);
-                    double velY = packet.getVelocityY() / 8000.0 * (vertical.getValue() / 100.0);
-                    double velZ = packet.getVelocityZ() / 8000.0 * (horizontal.getValue() / 100.0);
+    @Subscribe
+    public void onPacketReceive(PacketEvent.Receive event) {
+        if (mc.player == null || mc.world == null) return;
 
-                    mc.player.setVelocity(mc.player.getVelocity().add(
-                            velX - mc.player.getVelocity().x,
-                            velY - mc.player.getVelocity().y,
-                            velZ - mc.player.getVelocity().z
-                    ));
+        if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket packet &&
+                packet.getEntityId() == mc.player.getId()) {
 
-                    event.setCancelled(true);
-                } else if (mode.getValue() == Mode.NCP) {
-                    // Reduce velocity by NCP percentages
-                    double velX = packet.getVelocityX() / 8000.0 * (ncpHorizontal.getValue() / 100.0);
-                    double velY = packet.getVelocityY() / 8000.0 * (ncpVertical.getValue() / 100.0);
-                    double velZ = packet.getVelocityZ() / 8000.0 * (ncpHorizontal.getValue() / 100.0);
-
-                    mc.player.setVelocity(mc.player.getVelocity().add(
-                            velX - mc.player.getVelocity().x,
-                            velY - mc.player.getVelocity().y,
-                            velZ - mc.player.getVelocity().z
-                    ));
-
-                    event.setCancelled(true);
-                } else if (mode.getValue() == Mode.GRIM || mode.getValue() == Mode.GRIM_V3) {
-                    if (mode.getValue() == Mode.GRIM || (mode.getValue() == Mode.GRIM_V3 && !wasHurt)) {
-                        event.setCancelled(true);
-                    }
-                } else if (mode.getValue() == Mode.WALLS && isTouchingWall(mc.player)) {
-                    event.setCancelled(true);
+            if (mode.getValue().equals(Mode.CANCEL)) {
+                event.cancel();
+            } else if (mode.getValue().equals(Mode.REDUCE)) {
+                double vx = (packet.getVelocityX() / 8000.0) * (horizontal.getValue() / 100.0);
+                double vy = (packet.getVelocityY() / 8000.0) * (vertical.getValue() / 100.0);
+                double vz = (packet.getVelocityZ() / 8000.0) * (horizontal.getValue() / 100.0);
+                mc.player.setVelocity(vx, vy, vz);
+                event.cancel();
+            } else if (mode.getValue().equals(Mode.LEGIT)) {
+                if (mc.player.hurtTime > 0) {
+                    double vx = (packet.getVelocityX() / 8000.0) * 0.4;
+                    double vy = (packet.getVelocityY() / 8000.0) * 0.4;
+                    double vz = (packet.getVelocityZ() / 8000.0) * 0.4;
+                    mc.player.setVelocity(vx, vy, vz);
+                    event.cancel();
+                }
+            } else if (mode.getValue().equals(Mode.GRIM)) {
+                if (mc.player.hurtTime > 0) {
+                    event.cancel();
+                }
+            } else if (mode.getValue().equals(Mode.WALLS)) {
+                if (isTouchingWall(mc.player)) {
+                    event.cancel();
                 }
             }
         }
 
-        if (event.getPacket() instanceof ExplosionS2CPacket explosion) {
-            if (cancelExplosions.getValue()) {
-                if (mode.getValue() == Mode.NORMAL) {
-                    Vec3d current = mc.player.getVelocity();
-                    mc.player.setVelocity(
-                            current.x * (horizontal.getValue() / 100.0),
-                            current.y * (vertical.getValue() / 100.0),
-                            current.z * (horizontal.getValue() / 100.0)
-                    );
-                } else if (mode.getValue() == Mode.NCP) {
-                    Vec3d current = mc.player.getVelocity();
-                    mc.player.setVelocity(
-                            current.x * (ncpHorizontal.getValue() / 100.0),
-                            current.y * (ncpVertical.getValue() / 100.0),
-                            current.z * (ncpHorizontal.getValue() / 100.0)
-                    );
-                } else if (mode.getValue() == Mode.GRIM || (mode.getValue() == Mode.GRIM_V3 && !wasHurt)) {
-                    event.setCancelled(true);
-                } else if (mode.getValue() == Mode.WALLS && isTouchingWall(mc.player)) {
-                    event.setCancelled(true);
+        if (event.getPacket() instanceof ExplosionS2CPacket) {
+            if (mode.getValue().equals(Mode.CANCEL)) {
+                event.cancel();
+            } else if (mode.getValue().equals(Mode.REDUCE)) {
+                Vec3d vel = mc.player.getVelocity();
+                mc.player.setVelocity(
+                        vel.x * (horizontal.getValue() / 100.0),
+                        vel.y * (vertical.getValue() / 100.0),
+                        vel.z * (horizontal.getValue() / 100.0)
+                );
+            } else if (mode.getValue().equals(Mode.LEGIT)) {
+                if (mc.player.hurtTime > 0) {
+                    Vec3d vel = mc.player.getVelocity();
+                    mc.player.setVelocity(vel.multiply(0.4, 0.4, 0.4));
+                }
+            } else if (mode.getValue().equals(Mode.GRIM)) {
+                if (mc.player.hurtTime > 0) {
+                    event.cancel();
+                }
+            } else if (mode.getValue().equals(Mode.WALLS)) {
+                if (isTouchingWall(mc.player)) {
+                    event.cancel();
                 }
             }
         }
-    };
+    }
 
     @Override
-    public void onTick() {
-        if (mode.getValue() == Mode.GRIM_V3) {
-            wasHurt = mc.player.hurtTime > 0;
+    public void onUpdate() {
+        if (mc.player != null && noPush.getValue()) {
+            mc.player.noClip = true;  // Prevents block pushing
         }
     }
 
     private boolean isTouchingWall(ClientPlayerEntity player) {
-        return player.horizontalCollision;
+        return player.horizontalCollision || player.isInsideWall();
+    }
+
+    @Override
+    public void onDisable() {
+        if (mc.player != null) {
+            mc.player.noClip = false;
+        }
     }
 }
