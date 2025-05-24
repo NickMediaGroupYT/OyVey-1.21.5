e3632677941c631705c18e7571b4844f0afcdf8f
rtoue <rtoue@NICKPC>
2025-05-24 18:39:18 -0400

    OyVey ported to 1.21.5 working on adding modules as of right now

--- src/main/java/me/alpha432/oyvey/features/modules/player/PearlPhase.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/PearlPhase.java
@@ -2,60 +2,71 @@ package me.alpha432.oyvey.features.modules.player;
 
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 
 public class PearlPhase extends Module {
-    public Setting<Boolean> swing = register(new Setting<>("Swing", true));
-    public Setting<Float> pitch = register(new Setting<>("Pitch", 86f, 70f, 90f));
+
+    private final Setting<Boolean> swing = register(new Setting<>("Swing", true));
+    private final Setting<Integer> pitch = register(new Setting<>("Pitch", 86, 70, 90));
 
     public PearlPhase() {
-        super("PearlPhase", "", Category.PLAYER, true, false, false);
+        super("PearlPhase", "Phases into blocks with pearl", Category.PLAYER, true, false, false);
     }
 
     @Override
     public void onEnable() {
         if (mc.player == null || mc.world == null) {
-            toggle();
+            disable();
             return;
         }
 
         int pearlSlot = findPearlSlot();
         if (pearlSlot == -1) {
-            toggle();
+            disable();
             return;
         }
 
+        // Rotate to face block backward if sneaking
         float yaw = mc.player.getYaw();
         if (mc.options.backKey.isPressed()) {
-            yaw -= 180f;
+            yaw = yaw + 180;
         }
 
+        float p = pitch.getValue().floatValue();
+        rotate(yaw, p);
+
+        // Swap to pearl slot
         int prevSlot = mc.player.getInventory().getSelectedSlot();
         mc.player.getInventory().setSelectedSlot(pearlSlot);
 
-        // Apply rotation client-side
-        mc.player.setYaw(yaw);
-        mc.player.setPitch(pitch.getValue());
-
-        // Send pearl throw packet with yaw and pitch (1.21.5 requirement)
-        mc.getNetworkHandler().sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, (int) mc.world.getTime(), yaw, pitch.getValue()));
-
+        // Throw the pearl
+        mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, (int) mc.world.getTime(), yaw, pitch.getValue()));
         if (swing.getValue()) {
             mc.player.swingHand(Hand.MAIN_HAND);
         }
 
+        // Restore previous slot
         mc.player.getInventory().setSelectedSlot(prevSlot);
-        toggle(); // disable after use
+
+        disable();
     }
 
     private int findPearlSlot() {
         for (int i = 0; i < 9; i++) {
-            if (mc.player.getInventory().getStack(i).getItem() == Items.ENDER_PEARL) {
+            ItemStack stack = mc.player.getInventory().getStack(i);
+            if (stack.getItem() == Items.ENDER_PEARL) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private void rotate(float yaw, float pitch) {
+        mc.player.setYaw(yaw);
+        mc.player.setPitch(pitch);
     }
 }
